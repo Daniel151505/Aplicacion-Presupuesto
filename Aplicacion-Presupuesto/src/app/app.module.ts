b701c9b8269f34ad63e51f4f5b71a4c19dc503ad
Daniel151505 <daniel150520011@gmail.com>
2021-02-20 12:53:38 -0500

    Agregando Componente Listar Gasto

--- Aplicacion-Presupuesto/src/app/app.module.ts
+++ Aplicacion-Presupuesto/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GastosComponent } from './components/gastos/gastos.component';
 import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
 import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GastosComponent,
     IngresarPresupuestoComponent,
-    IngresarGastoComponent
+    IngresarGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule,
