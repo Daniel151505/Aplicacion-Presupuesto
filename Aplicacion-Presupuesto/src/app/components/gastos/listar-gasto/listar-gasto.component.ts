0493f08313a87fde0929b4e9d138946727515bb3
Daniel151505 <daniel150520011@gmail.com>
2021-02-20 18:47:28 -0500

    Importando Servicio Presupuesto Service

--- Aplicacion-Presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ Aplicacion-Presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-listar-gasto',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarGastoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private presupuestoService: PresupuestoService) { 
+ 
+  }
 
   ngOnInit(): void {
   }
