e9e7c0ac534a21c4599550198fce485d218b886a
Daniel151505 <daniel150520011@gmail.com>
2021-02-20 18:52:31 -0500

    Agregando Metodo ngOnDestroy

--- Aplicacion-Presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ Aplicacion-Presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
@@ -7,15 +7,19 @@ import { PresupuestoService } from 'src/app/services/presupuesto.service';
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent implements OnInit {
+export class ListarGastoComponent implements OnInit, OnDestroy {
 
   subscription: Subscription
 
   constructor(private presupuestoService: PresupuestoService) { 
- 
   }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+
 
 }
