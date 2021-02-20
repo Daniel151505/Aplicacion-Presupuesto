9994d548ec22e43592d9b1bc850c1384c01c7c17
Daniel151505 <daniel150520011@gmail.com>
2021-02-20 16:04:21 -0500

    Agregando Variables

--- Aplicacion-Presupuesto/src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
+++ Aplicacion-Presupuesto/src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresarPresupuestoComponent implements OnInit {
 
+  cantidad: number = 0
+  cantidadIncorrecta:boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
