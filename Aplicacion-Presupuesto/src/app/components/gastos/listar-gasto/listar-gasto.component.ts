44aab22aebc9d79c08ea975e8de9a9b7e979815a
Daniel151505 <daniel150520011@gmail.com>
2021-02-20 19:03:35 -0500

    Agregando Variables

--- Aplicacion-Presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ Aplicacion-Presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -9,9 +9,14 @@ import { PresupuestoService } from 'src/app/services/presupuesto.service';
 })
 export class ListarGastoComponent implements OnInit, OnDestroy {
 
+  presupuesto: number
+  restante:number
   subscription: Subscription
 
   constructor(private presupuestoService: PresupuestoService) { 
+     this.subscription = this.presupuestoService.getGastos().subscribe(data => {
+
+     })
   }
 
   ngOnInit(): void {
