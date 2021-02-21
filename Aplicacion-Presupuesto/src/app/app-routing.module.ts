e306aab5cda0264f08088975bb36c93cad658759
Daniel151505 <daniel150520011@gmail.com>
2021-02-20 21:08:23 -0500

    Fix error

--- Aplicacion-Presupuesto/src/app/app-routing.module.ts
+++ Aplicacion-Presupuesto/src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ const routes: Routes = [
   {path: '' , redirectTo: '/ingresarPresupuesto', pathMatch: 'full'},
   {path: 'ingresarPresupuesto' , component: IngresarPresupuestoComponent},
   {path: 'gastos', component: GastosComponent},
-  {path: '**', redirectTo: '/ingresarPresupueto', pathMatch: 'full'}
+  { path: '**', component: IngresarPresupuestoComponent },
 ];
 
-@NgModule({
+ @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
